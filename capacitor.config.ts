201031107c471a11122c0cd09be2283715ba9ff6
Michael Souto <michaelsouto@outlook.com>
2023-02-19 12:44:35 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'historapp-mob-front',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
